b8a6b8a8bdfd4a3a8d8fae2a83fc6870ee11e8b4
Shubham0701 <sshubh0701@gmail.com>
2017-08-09 01:05:14 +0530

    Update MyStack.js

--- MyStack.js
+++ MyStack.js
@@ -1,10 +1,19 @@
-var arr = new Array();
-var top=-1;
+var arr = [];
+var topVal=-1;
 function pushItem(){
 	var input = document.getElementById("push").value;
-	arr[++top] = input;
+	document.getElementById("push").value="";
+	arr[++topVal] = input;
+	console.log(arr[topVal] + " " + topVal);
 }
 
 function popItem(){
-	document.getElementById("spanPop").innerHTML = arr[top--];
+	document.getElementById("spanPop").innerHTML = arr[topVal--];
+	console.log(topVal);
+}
+
+function printStack(){
+	for(var index=0;index <= topVal ; index++){
+		document.getElementById("spanStack").innerHTML += arr[index] + " ";
+	}
 }
